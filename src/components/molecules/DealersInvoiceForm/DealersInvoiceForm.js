4670e1295865e44977f533d5219d91988a9d2408
Rashmi Shree S <rashmishrees@Rashmis-MacBook-Pro.local>
2022-08-26 18:46:58 +0530

    popppre

--- src/components/molecules/DealersInvoiceForm/DealersInvoiceForm.js
+++ src/components/molecules/DealersInvoiceForm/DealersInvoiceForm.js
@@ -70,7 +70,7 @@ console.log("dealersdata",dealersdata);
 console.log("changeddata",changeddata);
 console.log("products",products, products.productname, products.length);
 
-    if (products.productname.length == 0){
+    if (products.length == 0){
       productError = "Please Choose Atleast One Product";
     }
     if(!changeddata){
