15441cc3867d0f1d5b41dde700c2bb3395a9cec6
Rashmi Shree S <rashmishrees@Rashmis-MacBook-Pro.local>
2022-09-24 18:14:49 +0530

    ou

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import {
   Route
 } from 'react-router-dom';
 import axios from 'axios';
-import {useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 const api = axios.create({
   baseURL: `http://3.84.110.201:3001/`
 })
@@ -22,54 +22,52 @@ function App() {
   const adminloggedinevent = (e) => {
     setadminloggedin(e[0].username)
   }
-  console.log("logoutbtn",logoutbtn);
+  console.log("logoutbtn", logoutbtn);
   const logoutbuttonevent = (e) => {
     setlogoutbtn(e);
   }
   useEffect(() => {
     setlogoutbtn(JSON.parse(window.localStorage.getItem('logoutbtn')));
   }, []);
-
   useEffect(() => {
     window.localStorage.setItem('logoutbtn', logoutbtn);
   }, [logoutbtn]);
   return (
     <Router>
-        <Routes>
-          <Route path="/" element={<LoginPage 
-                                      api={api} 
-                                      logoutbuttonevent={logoutbuttonevent}
-                                      adminloggedinevent={adminloggedinevent}
-                                    />
-                                  }
-          />
-          <Route path="/main" element={<NavigationTiles
-                                        adminloggedin={adminloggedin}            
-                                      />}
-          />
-          <Route path="/viewinvoice" element={<ViewInvoicePage
-                                        api={api}
-                                      />}
-          />
-          <Route path="/addcustomerpage" element={<AddCustomerPage
-                                          api={api}
-                                        />}
-          />
-          <Route path="/generateinvoice" element={<GenerateInvoice
-                                          api={api}
-                                        />}/>
-          <Route path="/generatereport" element={<GenerateReport
-                                          api={api}
-                                        />}/>
-          <Route path="/employeemanagement" element={<EmployeeManagementPage
-                                          api={api}
-                                        />}/>
-        </Routes>
-        <div>
-          <Footer />
-        </div>
-      </Router>
+      <Routes>
+        <Route path="/" element={<LoginPage
+          api={api}
+          logoutbuttonevent={logoutbuttonevent}
+          adminloggedinevent={adminloggedinevent}
+        />
+        }
+        />
+        <Route path="/main" element={<NavigationTiles
+          adminloggedin={adminloggedin}
+        />}
+        />
+        <Route path="/viewinvoice" element={<ViewInvoicePage
+          api={api}
+        />}
+        />
+        <Route path="/addcustomerpage" element={<AddCustomerPage
+          api={api}
+        />}
+        />
+        <Route path="/generateinvoice" element={<GenerateInvoice
+          api={api}
+        />} />
+        <Route path="/generatereport" element={<GenerateReport
+          api={api}
+        />} />
+        <Route path="/employeemanagement" element={<EmployeeManagementPage
+          api={api}
+        />} />
+      </Routes>
+      <div>
+        <Footer />
+      </div>
+    </Router>
   );
 }
-
 export default App;
