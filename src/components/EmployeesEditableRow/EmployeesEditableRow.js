0734cd34eb088691c6c67a8a6a7071473f355f70
Rashmi Shree S <rashmishrees@Rashmis-MacBook-Pro.local>
2022-09-24 17:04:27 +0530

    ou

--- src/components/EmployeesEditableRow/EmployeesEditableRow.js
+++ src/components/EmployeesEditableRow/EmployeesEditableRow.js
@@ -8,44 +8,40 @@ const EmployeesEditableRow = ({
     handleCancelClick,
     handleEditFormSubmit
 }) => {
-    return(
+    return (
         <tr>
-          <td>
-            <input
-              type="text"
-              required="required"
-              placeholder="Enter a name..."
-              name="username"
-              defaultValue={rowdata.username}
-              onChange={handleEditFormChange}
-            ></input>
-          </td>
-          <td>
-            <input
-              type="text"
-              required="required"
-              placeholder="Enter a password..."
-              name="password"
-              defaultValue={rowdata.password}
-              onChange={handleEditFormChange}
-            ></input>
-          </td>
-          <td>
-            {/* <button type="submit">Save</button> */}
-            <CustomizedSaveIcon 
-                type="submit"
-                onClick={handleEditFormSubmit}
-            />
-            <CustomizedCancelIcon 
-                type="button"
-                onClick={handleCancelClick}
-            />
-            {/* <button type="button" 
-                onClick={handleCancelClick}
-                >
-              Cancel
-            </button> */}
-          </td>
+            <td>
+                <input
+                    type="text"
+                    required="required"
+                    placeholder="Enter a name..."
+                    name="username"
+                    defaultValue={rowdata.username}
+                    onChange={handleEditFormChange}
+                ></input>
+            </td>
+            <td>
+                <input
+                    type="text"
+                    required="required"
+                    placeholder="Enter a password..."
+                    name="password"
+                    defaultValue={rowdata.password}
+                    onChange={handleEditFormChange}
+                ></input>
+            </td>
+            <td>
+                <div className='editdeletecontainer'>
+                    <CustomizedSaveIcon
+                        type="submit"
+                        onClick={handleEditFormSubmit}
+                    />
+                    <CustomizedCancelIcon
+                        type="button"
+                        onClick={handleCancelClick}
+                    />
+                </div>
+            </td>
         </tr>
     );
 }
