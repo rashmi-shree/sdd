854b6bae7d8c6fe3db50c66698daf54e6b2d20ac
Rashmi Shree S <rashmishrees@Rashmis-MacBook-Pro.local>
2022-09-24 12:28:57 +0530

    ou

--- src/pages/EmployeeManagementPage/EmployeeManagementPage.js
+++ src/pages/EmployeeManagementPage/EmployeeManagementPage.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import {useNavigate} from 'react-router-dom';
 
 const EmployeeManagementPage = () => {
+    let navigate = useNavigate();
+    const [check, setcheck] = useState(false);
+    useEffect(() => {
+      setcheck(JSON.parse(window.localStorage.getItem('logoutbtn')));
+    }, []);
+    useEffect(()=>{
+      if(check == null){
+        navigate('/');
+      }
+    },[check])
     return(
         <div>employee management</div>
     );
